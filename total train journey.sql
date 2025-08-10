e8afb35bb302173d1a0b4517fd9e5e5ee0c59512
Harsh <94978520+Harsh24081990@users.noreply.github.com>
2025-08-10 20:52:15 +0530

    Update total train journey.sql

--- total train journey.sql
+++ total train journey.sql
@@ -1,9 +1,9 @@
 Input: ( I want total distance covered by each train between chennai to delhi) -------- 
 Source_city 	Dest_city 	Distance 	Train 
 Chennai 		   Nagpur 		   15000 		   A 
-Nagpur 			   Delhi 		    12000 		   A 
-Chennai 		   UP 			      17000 		   B 
-UP 				      Delhi 		    6000 		    B
+Nagpur 			   Delhi 		   12000 		   A 
+Chennai 		   UP 			    17000 		   B 
+UP 				     Delhi 		 6000 		    B
  
 output: ------- 
 Source_City  Dest_City  Train  Distance 
